7e7040e40f0ef4715fd3d04eb3a0b6f9e6b28375
T-yz <153387172+T-yz@users.noreply.github.com>
2024-03-05 20:04:24 +0000

    Add files via upload
    better UX as now age has a small remark instead of just printing.

--- test_app.py
+++ test_app.py
@@ -1,3 +1,11 @@
 user_name = input("what is your name? ")
-user_age = input("How old are you? ")
-print (f"Hello {user_name} \nAge: {user_age}")
+user_age = int(input("How old are you? "))
+print (f"Hello {user_name}")
+if user_age <= 15:
+    print("woah, you are young")
+elif user_age <= 25:
+    print("Hitting your prime")
+elif user_age <= 40:
+    print("Becoming wise")
+else:
+    print("you are a wise old man")
